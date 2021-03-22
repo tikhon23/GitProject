12d0324f3e79fcccc963c9483ee1070a665e41a7
Tikhon23 <bartemiys23@gmail.com>
2021-03-22 18:17:13 +0300

    Исправил замечание

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hello,world");// write your code here
+        System.out.println("hello,world");
     }
 }
